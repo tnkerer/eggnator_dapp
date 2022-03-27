fb0bef0c9c3cd2ef6b5dbdbebb4e3671bf0fa10d
menezesphill <phillipe.o.m@hotmail.com>
2022-03-26 22:54:14 -0300

    added rinkeby config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { projectId, mnemonic } = require('./secrets.json');
 
 /**
  * Use this file to configure your truffle project. It's seeded with some
@@ -39,12 +41,23 @@ module.exports = {
 
   networks: {
     
-      development: {
+    development: {
         host: "192.168.0.9",
         port: 7545,
         network_id: "5777", // Match any network id
       
     },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
+
 
     // Another network with more advanced options...
     // advanced: {
